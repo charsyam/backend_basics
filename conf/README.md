6f96acb8dd1c072529cab0bfd07fb9a4aad9699a
Ubuntu <junk3843@naver.com>
2017-03-22 18:53:23 +0000

    Write soma notes

---
+++ conf/README.md
@@ -0,0 +1,9 @@
+# conf
+
+1. sh deploy_nginx_conf.sh # when `soma01`
+2. sh run_redis.sh # when `soma02`
+3. sh run_zookeeper.sh start # `soma03`
+  * You can connect to zookeeper server
+    - sh ~/backend_basics/zookeeper/bin/zkCli.sh
+  * You can set sample directories of zookeeper
+    - python ~/backend_basics/soma03/prepare.py
---
+++ set_language.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo 'export LANGUAGE=en_US.UTF-8' >> ~/.bashrc
+echo 'export LC_ALL=en_US.UTF-8' >> ~/.bashrc
+sudo locale-gen en_US.UTF-8
+
+echo "===================================================="
+echo "RUN source ~/.bashrc"
+echo "RUN ./package_install.sh"
+echo "===================================================="
