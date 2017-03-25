75cc194bdd210ae38f265fc4a1badeaf69c475c2
hyonzin <hyeonjin507@gmail.com>
2017-03-25 12:55:35 +0900

    say thanks

--- set_language.sh
+++ set_language.sh
@@ -8,3 +8,7 @@ echo "===================================================="
 echo "RUN source ~/.bashrc"
 echo "RUN ./package_install.sh"
 echo "===================================================="
+
+echo
+echo "thanks"
+echo
